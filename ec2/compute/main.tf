54ace8ef6aaf5b53f64f647c497b5530ee643ec6
nikhildarocha <niks77883@gmail.com>
2020-09-17 12:16:37 +0530

    @ndarocha formatting

--- ec2/compute/main.tf
+++ ec2/compute/main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
-    region = var.aws_region
-    shared_credentials_file = var.aws_secret_file
-    profile = var.aws_secret_profile
+  region                  = var.aws_region
+  shared_credentials_file = var.aws_secret_file
+  profile                 = var.aws_secret_profile
 
 }
 
 resource "aws_instance" "es_cluster" {
-    ami =  lookup(var.ami,var.aws_region)
-    instance_type = var.instance_type
-    count = var.instance_count
+  ami           = lookup(var.ami, var.aws_region)
+  instance_type = var.instance_type
+  count         = var.instance_count
 
-    tags = {
+  tags = {
     Name = element(var.instance_tags, count.index)
-    }
+  }
 
 }
--- ec2/compute/variables.tf
+++ ec2/compute/variables.tf
@@ -1,39 +1,39 @@
 variable "aws_region" {
-    description = "AWS Region"
-    default = "us-east-1"
+  description = "AWS Region"
+  default     = "us-east-1"
 
 }
 variable "aws_secret_file" {
-    description = "AWS Secrets to authenticate"
-    default = "/Users/ndarocha/.aws/credentials"
+  description = "AWS Secrets to authenticate"
+  default     = "/Users/ndarocha/.aws/credentials"
 
 }
 
 variable "aws_secret_profile" {
-    description = "AWS Secrets profile"
-    default = "terraform"
+  description = "AWS Secrets profile"
+  default     = "terraform"
 }
 
 variable "instance_count" {
-    description = "Number of ec2 instances to launch"
-    default = "2"
+  description = "Number of ec2 instances to launch"
+  default     = "2"
 }
 
 variable "instance_type" {
-    description = "Type of the ec2 instance"
-    default = "t2.micro"
+  description = "Type of the ec2 instance"
+  default     = "t2.micro"
 }
 
 variable "ami" {
-    description = "Image for the ec2 instance"
-    type = map(string)
-    default = {
-        us-east-1 =  "ami-05e00da24aba682c3"
-    }
+  description = "Image for the ec2 instance"
+  type        = map(string)
+  default = {
+    us-east-1 = "ami-05e00da24aba682c3"
+  }
 }
 
 variable "instance_tags" {
-    description = "Tags for ec2 instance"
-    type = list(string)
-    default = ["es-1", "es-2"]
+  description = "Tags for ec2 instance"
+  type        = list(string)
+  default     = ["es-1", "es-2"]
 }
